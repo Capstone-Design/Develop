a39ab6dc6127a822b8da4cf8fc62517351357968
HOME-PC\home <xdv321@naver.com>
2018-05-04 01:39:56 +0900

    SettingValue 클래스 및 해상도, 화면 크기 관련 메소드 구현

--- windows/TIMPOITER/TIMPOITER/MainPage.xaml.cs
+++ windows/TIMPOITER/TIMPOITER/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,7 +14,8 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.UI.Input.Preview.Injection;
-
+using Windows.UI.ViewManagement;
+using Windows.Graphics.Display;
 
 namespace TIMPOITER
 {
@@ -22,52 +24,143 @@ namespace TIMPOITER
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private SettingValue aa = new SettingValue();
         public MainPage()
         {
             this.InitializeComponent();
+
+            aa.GetResolution();
         }
+
+        
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            InputInjector inputInjector = InputInjector.TryCreate();
+            //var info = new InjectedInputMouseInfo();
+            //info.MouseOptions = InjectedInputMouseOptions.Move;
+            //info.DeltaX = 100;
 
+            //InputInjector inputInjector = InputInjector.TryCreate();
+            //inputInjector.InjectMouseInput(new[] { info });
+
+            InputInjector inputInjector = InputInjector.TryCreate();
+            
             if (inputInjector != null)
             {
                 try
                 {
-                    inputInjector.InitializeTouchInjection(InjectedInputVisualizationMode.Indirect);
+                    inputInjector.InitializeTouchInjection(
+                        InjectedInputVisualizationMode.Default);
+
+                   
+                    inputInjector.InjectTouchInput(
+                        new List<InjectedInputTouchInfo>
+                    {
+                new InjectedInputTouchInfo
+                {
+                    Contact = new InjectedInputRectangle {
+                        Top = 500, Bottom = 500, Left = 400, Right = 400 },
+                    PointerInfo = new InjectedInputPointerInfo
+                    {
+                        
+                        PixelLocation = new InjectedInputPoint
+                        {
+                            PositionX = 400, PositionY = 500
+                        },
+                        PointerOptions = InjectedInputPointerOptions.InContact,
+                        PointerId = 2
+                    },
+                    Pressure = 1.0,
+                    TouchParameters =
+                    InjectedInputTouchParameters.Pressure |
+                    InjectedInputTouchParameters.Contact
+                }
+                    });
+                    
+                   // System.Threading.Tasks.Task.Delay(10).Wait();
 
                     inputInjector.InjectTouchInput(
-                        new List<InjectedInputTouchInfo>{
-                            new InjectedInputTouchInfo{
-                                Contact = new InjectedInputRectangle {Top = 150, Bottom = 150, Left = 40, Right = 40 },
-                                PointerInfo = new InjectedInputPointerInfo{
-                                    PixelLocation = new InjectedInputPoint{PositionX = 150, PositionY = 150},
-                                    PointerOptions = InjectedInputPointerOptions.InContact,
-                                    PointerId = 1
-                                 },
-                            Pressure = 1.0,
-                            TouchParameters = InjectedInputTouchParameters.Pressure | InjectedInputTouchParameters.Contact
-                            }
-                        });
+                        new List<InjectedInputTouchInfo> {
+                new InjectedInputTouchInfo {
+                    Contact = new InjectedInputRectangle
+                    {
+                        Top = 500,
+                        Bottom = 500,
+                        Left = 400,
+                        Right = 400
+                    },
+                    PointerInfo = new InjectedInputPointerInfo {
+                        PixelLocation = new InjectedInputPoint {
+                            PositionX = 400, PositionY = 500
+                        },
+                        PointerOptions = InjectedInputPointerOptions.PointerUp,
+                        PointerId = 2,
+                    },
+                    Pressure = 0.0,
+                    TouchParameters =
+                    InjectedInputTouchParameters.Pressure |
+                    InjectedInputTouchParameters.Contact
+                }
+                    });
                 }
                 catch (ArgumentException args)
                 {
-                    // Handle exception.
+                    
                 }
             }
         }
     }
 
-    public class touch
+    public class SettingValue
     {
-        public
-            //vector<long, int, int> distance;
-	        //vector<int, int> start;
-	        //InjectedInputTouchInfo contact;   //터치 포인터 클래스 
-            InputInjector inputInjector = InputInjector.TryCreate();
 
-        private touch()
+        private int[] screensize = new int[2];
+        private int[] resolution = new int[2];
+        private double[] calibration = new double[2];
+        //arduino = new [2];
+        private int firstStart;
+
+        //해상도가 변경되었을 때의 설정 변경
+        public void ResolutionChanged()
+        {
+
+        }
+
+        //디스플레이 사이즈 저장 
+        public void SetScreen(int[] size)
+        {
+            screensize[0] = size[0];
+            screensize[1] = size[1];
+        }
+
+        // 해상도 가져오기 및 저장 
+        public Size GetResolution()
+        {
+            var bounds = ApplicationView.GetForCurrentView().VisibleBounds;
+            var scaleFactor = DisplayInformation.GetForCurrentView().RawPixelsPerViewPixel;
+            var size = new Size(bounds.Width * scaleFactor, bounds.Height * scaleFactor);
+            resolution[0] = (int)size.Height;
+            resolution[1] = (int)size.Width;
+ 
+            return size;
+        }
+
+        /*public int* GetBattery()
+        {
+
+            return &; 
+        }*/
+    }
+
+    public class Touch
+    {
+        // public
+        //vector<long, int, int> distance;
+        //vector<int, int> start;
+        //InjectedInputTouchInfo contact;   //터치 포인터 클래스 
+        //InputInjector inputInjector = InputInjector.TryCreate();
+
+        public Touch()
         {
             //inputInjector.InitializeTouchInjection(InjectedInputVisualizationMode.Indirect);
             //inputInjector.InjectTouchInput(new List<InjectedInputTouchInfo>
@@ -98,7 +191,7 @@ namespace TIMPOITER
             
         }
 
-        void TouchInput(char type, int x, int y)
+        public void TouchInput(char type, int x, int y)
         {
             
 
