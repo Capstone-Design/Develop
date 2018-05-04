2078a82e3a1017ea82a4ac8758c533037fc3ea7d
HOME-PC\home <xdv321@naver.com>
2018-05-04 15:25:39 +0900

    UI 관련  기초 구현

---
+++ windows/TIMPOITER/TIMPOITER/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.ApplicationModel.Background;
+using Windows.Storage;
+using Windows.UI.Xaml.Controls;
+
+namespace TIMPOITER
+{
+    public partial class MainPage : Page
+    {
+        public const string FEATURE_NAME = "TIMPOINTER SETTING";
+
+        List<Scenario> scenarios = new List<Scenario>
+        {
+            new Scenario() { Title="Screen Settting", ClassType=typeof(Scenario1_screensetting) },
+            new Scenario() { Title="Bluetooth Setting", ClassType=typeof(Scenario2_bluetooth) },
+            //new Scenario() { Title="Server: Publish foreground", ClassType=typeof(Scenario3_ServerForeground) },
+        };
+
+        public string SelectedBleDeviceId;
+        public string SelectedBleDeviceName = "No device selected";
+    }
+
+    public class Scenario
+    {
+        public string Title { get; set; }
+        public Type ClassType { get; set; }
+    }
+}
--- windows/TIMPOITER/TIMPOITER/MainPage.xaml.cs
+++ windows/TIMPOITER/TIMPOITER/MainPage.xaml.cs
@@ -24,92 +24,207 @@ namespace TIMPOITER
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public static MainPage Current;
+
         private SettingValue aa = new SettingValue();
         public MainPage()
         {
             this.InitializeComponent();
-
+            Current = this;
+            SampleTitle.Text = FEATURE_NAME;
             aa.GetResolution();
+            
         }
 
-        
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            //var info = new InjectedInputMouseInfo();
-            //info.MouseOptions = InjectedInputMouseOptions.Move;
-            //info.DeltaX = 100;
-
-            //InputInjector inputInjector = InputInjector.TryCreate();
-            //inputInjector.InjectMouseInput(new[] { info });
-
-            InputInjector inputInjector = InputInjector.TryCreate();
-            
-            if (inputInjector != null)
+            // Populate the scenario list from the SampleConfiguration.cs file
+            ScenarioControl.ItemsSource = scenarios;
+            if (Window.Current.Bounds.Width < 640)
             {
-                try
-                {
-                    inputInjector.InitializeTouchInjection(
-                        InjectedInputVisualizationMode.Default);
+                ScenarioControl.SelectedIndex = -1;
+            }
+            else
+            {
+                ScenarioControl.SelectedIndex = 0;
+            }
+        }
 
-                   
-                    inputInjector.InjectTouchInput(
-                        new List<InjectedInputTouchInfo>
-                    {
-                new InjectedInputTouchInfo
-                {
-                    Contact = new InjectedInputRectangle {
-                        Top = 500, Bottom = 500, Left = 400, Right = 400 },
-                    PointerInfo = new InjectedInputPointerInfo
-                    {
-                        
-                        PixelLocation = new InjectedInputPoint
-                        {
-                            PositionX = 400, PositionY = 500
-                        },
-                        PointerOptions = InjectedInputPointerOptions.InContact,
-                        PointerId = 2
-                    },
-                    Pressure = 1.0,
-                    TouchParameters =
-                    InjectedInputTouchParameters.Pressure |
-                    InjectedInputTouchParameters.Contact
-                }
-                    });
-                    
-                   // System.Threading.Tasks.Task.Delay(10).Wait();
 
-                    inputInjector.InjectTouchInput(
-                        new List<InjectedInputTouchInfo> {
-                new InjectedInputTouchInfo {
-                    Contact = new InjectedInputRectangle
-                    {
-                        Top = 500,
-                        Bottom = 500,
-                        Left = 400,
-                        Right = 400
-                    },
-                    PointerInfo = new InjectedInputPointerInfo {
-                        PixelLocation = new InjectedInputPoint {
-                            PositionX = 400, PositionY = 500
-                        },
-                        PointerOptions = InjectedInputPointerOptions.PointerUp,
-                        PointerId = 2,
-                    },
-                    Pressure = 0.0,
-                    TouchParameters =
-                    InjectedInputTouchParameters.Pressure |
-                    InjectedInputTouchParameters.Contact
-                }
-                    });
-                }
-                catch (ArgumentException args)
+        private void ScenarioControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Clear the status block when navigating scenarios.
+            //NotifyUser(String.Empty, NotifyType.StatusMessage);
+
+            ListBox scenarioListBox = sender as ListBox;
+            Scenario s = scenarioListBox.SelectedItem as Scenario;
+            if (s != null)
+            {
+                ScenarioFrame.Navigate(s.ClassType);
+                if (Window.Current.Bounds.Width < 640)
                 {
-                    
+                    Splitter.IsPaneOpen = false;
                 }
             }
         }
+
+        public List<Scenario> Scenarios
+        {
+            get { return this.scenarios; }
+        }
+
+        //public void NotifyUser(string strMessage, NotifyType type)
+        //{
+        //    // If called from the UI thread, then update immediately.
+        //    // Otherwise, schedule a task on the UI thread to perform the update.
+        //    if (Dispatcher.HasThreadAccess)
+        //    {
+        //        UpdateStatus(strMessage, type);
+        //    }
+        //    else
+        //    {
+        //        var task = Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => UpdateStatus(strMessage, type));
+        //    }
+        //}
+
+        //private void UpdateStatus(string strMessage, NotifyType type)
+        //{
+        //    switch (type)
+        //    {
+        //        case NotifyType.StatusMessage:
+        //            StatusBorder.Background = new SolidColorBrush(Windows.UI.Colors.Green);
+        //            break;
+        //        case NotifyType.ErrorMessage:
+        //            StatusBorder.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+        //            break;
+        //    }
+
+        //    StatusBlock.Text = strMessage;
+
+        //    // Collapse the StatusBlock if it has no text to conserve real estate.
+        //    StatusBorder.Visibility = (StatusBlock.Text != String.Empty) ? Visibility.Visible : Visibility.Collapsed;
+        //    if (StatusBlock.Text != String.Empty)
+        //    {
+        //        StatusBorder.Visibility = Visibility.Visible;
+        //        StatusPanel.Visibility = Visibility.Visible;
+        //    }
+        //    else
+        //    {
+        //        StatusBorder.Visibility = Visibility.Collapsed;
+        //        StatusPanel.Visibility = Visibility.Collapsed;
+        //    }
+
+        //    // Raise an event if necessary to enable a screen reader to announce the status update.
+        //    var peer = FrameworkElementAutomationPeer.FromElement(StatusBlock);
+        //    if (peer != null)
+        //    {
+        //        peer.RaiseAutomationEvent(AutomationEvents.LiveRegionChanged);
+        //    }
+        //}
+
+        async void Footer_Click(object sender, RoutedEventArgs e)
+        {
+            await Windows.System.Launcher.LaunchUriAsync(new Uri(((HyperlinkButton)sender).Tag.ToString()));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Splitter.IsPaneOpen = !Splitter.IsPaneOpen;
+        }
     }
+
+    public class ScenarioBindingConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            Scenario s = value as Scenario;
+            return (MainPage.Current.Scenarios.IndexOf(s) + 1) + ") " + s.Title;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return true;
+        }
+    }
+
+
+
+    //    private void Button_Click(object sender, RoutedEventArgs e)
+    //    {
+    //        //var info = new InjectedInputMouseInfo();
+    //        //info.MouseOptions = InjectedInputMouseOptions.Move;
+    //        //info.DeltaX = 100;
+
+    //        //InputInjector inputInjector = InputInjector.TryCreate();
+    //        //inputInjector.InjectMouseInput(new[] { info });
+
+    //        InputInjector inputInjector = InputInjector.TryCreate();
+
+    //        if (inputInjector != null)
+    //        {
+    //            try
+    //            {
+    //                inputInjector.InitializeTouchInjection(
+    //                    InjectedInputVisualizationMode.Default);
+
+
+    //                inputInjector.InjectTouchInput(
+    //                    new List<InjectedInputTouchInfo>
+    //                {
+    //            new InjectedInputTouchInfo
+    //            {
+    //                Contact = new InjectedInputRectangle {
+    //                    Top = 500, Bottom = 500, Left = 400, Right = 400 },
+    //                PointerInfo = new InjectedInputPointerInfo
+    //                {
+
+    //                    PixelLocation = new InjectedInputPoint
+    //                    {
+    //                        PositionX = 400, PositionY = 500
+    //                    },
+    //                    PointerOptions = InjectedInputPointerOptions.InContact,
+    //                    PointerId = 2
+    //                },
+    //                Pressure = 1.0,
+    //                TouchParameters =
+    //                InjectedInputTouchParameters.Pressure |
+    //                InjectedInputTouchParameters.Contact
+    //            }
+    //                });
+
+    //               // System.Threading.Tasks.Task.Delay(10).Wait();
+
+    //                inputInjector.InjectTouchInput(
+    //                    new List<InjectedInputTouchInfo> {
+    //            new InjectedInputTouchInfo {
+    //                Contact = new InjectedInputRectangle
+    //                {
+    //                    Top = 500,
+    //                    Bottom = 500,
+    //                    Left = 400,
+    //                    Right = 400
+    //                },
+    //                PointerInfo = new InjectedInputPointerInfo {
+    //                    PixelLocation = new InjectedInputPoint {
+    //                        PositionX = 400, PositionY = 500
+    //                    },
+    //                    PointerOptions = InjectedInputPointerOptions.PointerUp,
+    //                    PointerId = 2,
+    //                },
+    //                Pressure = 0.0,
+    //                TouchParameters =
+    //                InjectedInputTouchParameters.Pressure |
+    //                InjectedInputTouchParameters.Contact
+    //            }
+    //                });
+    //            }
+    //            catch (ArgumentException args)
+    //            {
+
+    //            }
+    //        }
+    //    }
+    //}
 
     public class SettingValue
     {
@@ -141,7 +256,11 @@ namespace TIMPOITER
             var size = new Size(bounds.Width * scaleFactor, bounds.Height * scaleFactor);
             resolution[0] = (int)size.Height;
             resolution[1] = (int)size.Width;
- 
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = resolution[0].ToString();
+            TextBlock textBlock2 = new TextBlock();
+            textBlock2.Text = resolution[1].ToString();
+
             return size;
         }
 
@@ -198,4 +317,8 @@ namespace TIMPOITER
         }
 
     }
+
+   
+
+    
 }
