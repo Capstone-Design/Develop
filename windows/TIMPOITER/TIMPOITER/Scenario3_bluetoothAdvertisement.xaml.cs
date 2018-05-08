bf3817be1e52ab49aa3097e0909faa5226d773f1
HOME-PC\home <xdv321@naver.com>
2018-05-08 22:51:58 +0900

    서비스 uuid 필터링 추가

--- windows/TIMPOITER/TIMPOITER/Scenario3_bluetoothAdvertisement.xaml.cs
+++ windows/TIMPOITER/TIMPOITER/Scenario3_bluetoothAdvertisement.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.Storage.Streams;
+using Windows.Devices.Bluetooth;
 using Windows.Devices.Bluetooth.Advertisement;
 
 
@@ -36,11 +37,11 @@ namespace TIMPOITER
 
             // Create and initialize a new watcher instance.
             watcher = new BluetoothLEAdvertisementWatcher();
-            //Guid TimpointerServiceUUID = new Guid("00000xdfb0 - 0000 - 1000 - 8000 - 00805F9B34FB");
-            //Guid TimpointerCharacteristicUUID = new Guid("00000xdfb1 - 0000 - 1000 - 8000 - 00805F9B34FB");
+            Guid TimpointerServiceUUID = BluetoothUuidHelper.FromShortId(0xdfb0);
+            //Guid TimpointerCharacteristicUUID = BluetoothUuidHelper.FromShortId(0xdfb1);
 
-       
-            //watcher.AdvertisementFilter.Advertisement.ServiceUuids.Add(TimpointerServiceUUID);
+
+            watcher.AdvertisementFilter.Advertisement.ServiceUuids.Add(TimpointerServiceUUID);
 
 
             watcher.SignalStrengthFilter.InRangeThresholdInDBm = -70;
@@ -58,141 +59,91 @@ namespace TIMPOITER
         {
             rootPage = MainPage.Current;
 
-            // Attach a handler to process the received advertisement. 
-            // The watcher cannot be started without a Received handler attached
             watcher.Received += OnAdvertisementReceived;
 
-            // Attach a handler to process watcher stopping due to various conditions,
-            // such as the Bluetooth radio turning off or the Stop method was called
             watcher.Stopped += OnAdvertisementWatcherStopped;
 
-            // Attach handlers for suspension to stop the watcher when the App is suspended.
             App.Current.Suspending += App_Suspending;
             App.Current.Resuming += App_Resuming;
 
         }
 
-        /// <summary>
-        /// Invoked immediately before the Page is unloaded and is no longer the current source of a parent Frame.
-        /// </summary>
-        /// <param name="e">
-        /// Event data that can be examined by overriding code. The event data is representative
-        /// of the navigation that will unload the current Page unless canceled. The
-        /// navigation can potentially be canceled by setting Cancel.
-        /// </param>
         protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
         {
-            // Remove local suspension handlers from the App since this page is no longer active.
+         
             App.Current.Suspending -= App_Suspending;
             App.Current.Resuming -= App_Resuming;
 
-            // Make sure to stop the watcher when leaving the context. Even if the watcher is not stopped,
-            // scanning will be stopped automatically if the watcher is destroyed.
             watcher.Stop();
-            // Always unregister the handlers to release the resources to prevent leaks.
+
             watcher.Received -= OnAdvertisementReceived;
             watcher.Stopped -= OnAdvertisementWatcherStopped;
 
             base.OnNavigatingFrom(e);
         }
 
-        /// <summary>
-        /// Invoked when application execution is being suspended.  Application state is saved
-        /// without knowing whether the application will be terminated or resumed with the contents
-        /// of memory still intact.
-        /// </summary>
-        /// <param name="sender">The source of the suspend request.</param>
-        /// <param name="e">Details about the suspend request.</param>
         private void App_Suspending(object sender, Windows.ApplicationModel.SuspendingEventArgs e)
         {
-            // Make sure to stop the watcher on suspend.
+           
             watcher.Stop();
-            // Always unregister the handlers to release the resources to prevent leaks.
+            
             watcher.Received -= OnAdvertisementReceived;
             watcher.Stopped -= OnAdvertisementWatcherStopped;
 
         }
 
-        /// <summary>
-        /// Invoked when application execution is being resumed.
-        /// </summary>
-        /// <param name="sender">The source of the resume request.</param>
-        /// <param name="e"></param>
+   
         private void App_Resuming(object sender, object e)
         {
             watcher.Received += OnAdvertisementReceived;
             watcher.Stopped += OnAdvertisementWatcherStopped;
         }
 
-        /// <summary>
-        /// Invoked as an event handler when the Run button is pressed.
-        /// </summary>
-        /// <param name="sender">Instance that triggered the event.</param>
-        /// <param name="e">Event data describing the conditions that led to the event.</param>
         private void RunButton_Click(object sender, RoutedEventArgs e)
         {
-            // Calling watcher start will start the scanning if not already initiated by another client
             watcher.Start();
 
         }
 
-        /// <summary>
-        /// Invoked as an event handler when the Stop button is pressed.
-        /// </summary>
-        /// <param name="sender">Instance that triggered the event.</param>
-        /// <param name="e">Event data describing the conditions that led to the event.</param>
         private void StopButton_Click(object sender, RoutedEventArgs e)
         {
-            // Stopping the watcher will stop scanning if this is the only client requesting scan
             watcher.Stop();
-
         }
 
-        /// <summary>
-        /// Invoked as an event handler when an advertisement is received.
-        /// </summary>
-        /// <param name="watcher">Instance of watcher that triggered the event.</param>
-        /// <param name="eventArgs">Event data containing information about the advertisement event.</param>
+   
         private async void OnAdvertisementReceived(BluetoothLEAdvertisementWatcher watcher, BluetoothLEAdvertisementReceivedEventArgs eventArgs)
         {
-            // We can obtain various information about the advertisement we just received by accessing 
-            // the properties of the EventArgs class
-
-            // The timestamp of the event
+            
             DateTimeOffset timestamp = eventArgs.Timestamp;
 
-            // The type of advertisement
+       
             BluetoothLEAdvertisementType advertisementType = eventArgs.AdvertisementType;
 
-            // The received signal strength indicator (RSSI)
+        
             Int16 rssi = eventArgs.RawSignalStrengthInDBm;
 
-            // The local name of the advertising device contained within the payload, if any
+       
             string localName = eventArgs.Advertisement.LocalName;
 
-            // Check if there are any manufacturer-specific sections.
-            // If there is, print the raw data of the first manufacturer section (if there are multiple).
+       
             string manufacturerDataString = "";
             var manufacturerSections = eventArgs.Advertisement.ManufacturerData;
             if (manufacturerSections.Count > 0)
             {
-                // Only print the first one of the list
                 var manufacturerData = manufacturerSections[0];
                 var data = new byte[manufacturerData.Data.Length];
                 using (var reader = DataReader.FromBuffer(manufacturerData.Data))
                 {
                     reader.ReadBytes(data);
                 }
-                // Print the company ID + the raw data in hex format
                 manufacturerDataString = string.Format("0x{0}: {1}",
                     manufacturerData.CompanyId.ToString("X"),
                     BitConverter.ToString(data));
             }
 
-            // Serialize UI update to the main UI thread
+         
             await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
             {
-                // Display these information on the list
                 ReceivedAdvertisementListBox.Items.Add(string.Format("[{0}]: type={1}, rssi={2}, name={3}, manufacturerData=[{4}]",
                     timestamp.ToString("hh\\:mm\\:ss\\.fff"),
                     advertisementType.ToString(),
@@ -202,14 +153,8 @@ namespace TIMPOITER
             });
         }
 
-        /// <summary>
-        /// Invoked as an event handler when the watcher is stopped or aborted.
-        /// </summary>
-        /// <param name="watcher">Instance of watcher that triggered the event.</param>
-        /// <param name="eventArgs">Event data containing information about why the watcher stopped or aborted.</param>
         private async void OnAdvertisementWatcherStopped(BluetoothLEAdvertisementWatcher watcher, BluetoothLEAdvertisementWatcherStoppedEventArgs eventArgs)
         {
-            // Notify the user that the watcher was stopped
             await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
             {
                 
