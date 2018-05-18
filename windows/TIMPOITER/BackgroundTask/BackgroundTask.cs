c653960bbc3486b1f11a6ebf81897030e6d3e71d
HOME-PC\home <xdv321@naver.com>
2018-05-19 01:11:24 +0900

    터치 입력 백그라운드 성공   일정 시간 후 작동 안함

--- windows/TIMPOITER/BackgroundTask/BackgroundTask.cs
+++ windows/TIMPOITER/BackgroundTask/BackgroundTask.cs
@@ -11,29 +11,104 @@ using Windows.ApplicationModel.Background;
 using Windows.Devices.Bluetooth.Background;
 using Windows.Storage;
 using Windows.Storage.Streams;
+using Windows.UI.Input.Preview.Injection;
 
 
 namespace BackgroundTask
 {
     public sealed class BaseTask : IBackgroundTask
     {
+        private static InputInjector inputInjector;
+
 
         public void Run(IBackgroundTaskInstance taskInstance)
         {
-            
+            inputInjector = InputInjector.TryCreate();
+            // Default는 기본 터치 이펙트, Indirect는 원에 +표시가 있는 마크가 표시됨.
+            inputInjector.InitializeTouchInjection(InjectedInputVisualizationMode.Default);
 
             System.Diagnostics.Debug.WriteLine("back ground task sss");
+            Touch(1, 150, 170);
+            System.Threading.Tasks.Task.Delay(100).Wait();
+            Touch(2, 150, 170);
+            System.Threading.Tasks.Task.Delay(100).Wait();
+            Touch(1, 150, 170);
+            System.Threading.Tasks.Task.Delay(100).Wait();
+            Touch(2, 150, 170);
             ApplicationData.Current.LocalSettings.Values[taskInstance.Task.Name] = "성공";
 
           
         }
+
+
+        private int Touch(int type, double X, double Y)
+        {
+            InjectedInputPointerOptions pointerOption;
+            Double pressure;        // 0.0~ 1.0 (1024단계)
+            switch (type)
+            {
+                case 0:
+                    pointerOption = InjectedInputPointerOptions.InRange;
+                    pressure = 1.0;
+                    break;
+                case 1:
+                    pointerOption = InjectedInputPointerOptions.InContact;
+                    pressure = 1.0;
+                    break;
+                case 2:
+                    pointerOption = InjectedInputPointerOptions.PointerUp;
+                    pressure = 0;
+                    break;
+                default:
+                    return -1;
+            }
+            var location = new InjectedInputPoint { PositionX = (int)X, PositionY = (int)Y };
+
+            try
+            {
+                inputInjector.InjectTouchInput
+                (
+                    new List<InjectedInputTouchInfo>
+                    {
+                        new InjectedInputTouchInfo
+                        {
+                            Contact = new InjectedInputRectangle
+                            {
+                                Top = 50,
+                                Bottom = 50,
+                                Left = 40,
+                                Right = 40
+                            },
+                            PointerInfo = new InjectedInputPointerInfo
+                            {
+                                PixelLocation = location,
+                                PointerOptions = pointerOption,
+                                PointerId = 1,
+                            },
+                            Pressure = pressure,
+                            TouchParameters =
+                            InjectedInputTouchParameters.Pressure |
+                            InjectedInputTouchParameters.Contact
+                        }
+                    }
+                );
+                return 1;
+            }
+            catch (Exception e)
+            {
+                return -2;
+            }
+        }
+
     }
 
 
 
 
-    //BLE 통신 
-    public sealed class BLEScanTask : IBackgroundTask
+
+
+//BLE 통신 
+public sealed class BLEScanTask : IBackgroundTask
     {
         BackgroundTaskDeferral deferral;
 
